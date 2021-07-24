7ed8ed9fa0ccb3f63a9b59374b3ff3c7134d4926
suesanjp <a.drop.of.my.essence@gmail.com>
2021-07-24 21:34:27 +0900

    first commit

---
+++ KeePaasEdit.py
@@ -0,0 +1,11 @@
+# Edit all http to https on KeePaasXC
+# before Run this Search "http" then click first data
+
+import pyautogui
+
+pyautogui.position()
+# pyautogui.hotkey("commnad", "tab")
+# pyautogui.press("enter")
+pyautogui.press("tab", presses=3)
+
+# alert(text="Finished", title="F", button="OK")
