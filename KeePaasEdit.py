5cc2f80f05f86de0f40b68ca772fc1931d562b6d
suesanjp <a.drop.of.my.essence@gmail.com>
2021-07-24 23:18:36 +0900

    secound commit

--- KeePaasEdit.py
+++ KeePaasEdit.py
@@ -1,11 +1,20 @@
 # Edit all http to https on KeePaasXC
-# before Run this Search "http" then click first data
+# before Run this Search "http:" then click first data
 
 import pyautogui
+from time import sleep
 
-pyautogui.position()
-# pyautogui.hotkey("commnad", "tab")
-# pyautogui.press("enter")
-pyautogui.press("tab", presses=3)
+pyautogui.hotkey("command", "tab")
+for i in range(10):
+    pyautogui.press("enter")
+    pyautogui.press("tab", presses=3)
+    pyautogui.press("left")
+    pyautogui.press("right", presses=4)
+    pyautogui.press("s")
+    # print(pyautogui.position)で計測
+    pyautogui.click(x=1226, y=806)
+    sleep(1)
+    pyautogui.press("down")
+    sleep(1)
 
-# alert(text="Finished", title="F", button="OK")
+print("DONE")
